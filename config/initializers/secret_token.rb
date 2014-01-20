2ca02d2452f82be4a5e1a800c07d3dcb29e4b42e
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2014-01-20 14:01:41 -0600

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NosotrasCorremos::Application.config.secret_token = 'e13fcb98510e2adbcd9c03518e27b7956843de0574f066e0b5499b6a7f8630843d419ebe0c19abc0e293f2edd50ca8a68654160b5e235330ce389febe0810001'
