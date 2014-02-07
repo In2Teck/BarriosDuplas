0e6ae8cb54ca039b3e547001478b5f715a26b486
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2014-02-07 12:26:00 -0600

    Corregidos modales de invitaciones.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -329,8 +329,9 @@ function muestraRequests() {
     url: "/invitaciones_pendientes",
     data_type: "html",
     success: function(data, textStatus, jqXHR) {
-      var html = "<div id='sub_izq' class='profile_izq responsive_bck'></div><div id='sub_der' class='barrio_der responsive_bck'></div>"; 
+      var html = "<div id='sub_izq' class='profile_izq responsive_bck'><div class='menu_text menu_font'><div id='status' class='status'></div></div></div><div id='sub_der' class='barrio_der responsive_bck'></div>"; 
       modalDialogue(html);
+      $("#status").html($("#home-values").data("user").first_name.toUpperCase() + " " + $("#home-values").data("user").last_name.toUpperCase() + "<p class='km_chico'>" + $("#home-values").data("user").kilometers + " KM</p>");
       $(".profile_izq").css("background", "url('/assets/bg_gradient_perfil.png'), url('http://graph.facebook.com/"+ facebook_id +"/picture?redirect=1&type=square&width=300&height=300')");
       $("#sub_der").html(data);
     },
@@ -367,7 +368,7 @@ function muestraInvitacion() {
       url: "/invitar_amiga",
       data_type: "html",
       success: function(data, textStatus, jqXHR) {
-        var html = "<div id='sub_izq' class='amiga_izq responsive_bck'><div class='menu_text menu_font'><div id='status' class='status'></div></div></div></div><div id='sub_der' class='amiga_der responsive_bck'><div class='img_modal_amiga'></div></div>"; 
+        var html = "<div id='sub_izq' class='amiga_izq responsive_bck'></div><div id='sub_der' class='amiga_der responsive_bck'><div class='img_modal_amiga'></div></div>"; 
         modalDialogue(html);
         $("#sub_der").html(data);
         if (just_invited) {
@@ -378,9 +379,8 @@ function muestraInvitacion() {
           $("#invitacion_texto").css("display", "none");
         }
 
-        $("#status").html($("#home-values").data("user").first_name.toUpperCase() + " " + $("#home-values").data("user").last_name.toUpperCase() + "<p class='km_chico'>" + $("#home-values").data("user").kilometers + " KM</p>");
         $(".img_modal_amiga").css({background: "url('/assets/bg_gradient_perfil.png'), url('http://graph.facebook.com/"+ $("#home-values").data("invited").invited_user_facebook_id +"/picture?redirect=1&width=150&height=150')", "background-size": "100%", margin: "auto" });
-        $("#amiga_invitada").text($("#home-values").data("invited").invited_user_name.toUpperCase());
+        $("#amiga_invitada").text($("#home-values").data("invited").invited_user_name.toUpperCase().substr(0,18));
       },
       error: function() {
       } 
