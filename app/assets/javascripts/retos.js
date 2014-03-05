1c45bcc88b8860c5935eb8d53815252bce8523b0
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2014-03-05 11:14:22 -0600

    City truck actualizado.

--- app/assets/javascripts/retos.js
+++ app/assets/javascripts/retos.js
@@ -75,23 +75,23 @@ function muestraSocialRun(){
 
 function muestraCityTruck(){
   var html_izq = "<div id='runs_container' style='height: 100%;'><div class='sub_head'>SUMA TUS KILÓMETROS DE UNA MANERA DISTINTA.<br/>SÚBETE AL NIKE CITY TRUCK.</div>"+
+  "<div class='subtitle'>EL CAMIÓN PUEDE MOVERSE DE LUGAR SIN PREVIO AVISO</br>"+
+  "YA QUE SU ESTANCIA DEPENDE DE CADA DELEGACIÓN.</div>"+
   "<div class='text'>"+
-  "<p class='entre'>DOMINGO 23 DE FEBRERO<br/>"+
+  "<p class='entre'>MARTES 18 DE MARZO<br/>"+
+  "SEC. 29 ROJO GÓMEZ Y ESCUELA ANDERSEN - DE 11:00AM A 6:00PM.</p>"+
+  "<p class='entre'>JUEVES 20 DE MARZO<br/>"+
+  "CCH SUR Y ANDADOR COPILCO - DE 11:00AM A 6:00PM.</p>"+
+  "<p class='entre'>VIERNES 28 DE MARZO<br/>"+
+  "UAM XOCHIMILCO Y ALAMEDA DEL SUR - DE 11:00AM A 6:00PM.</p>"+
+  "<p class='entre'>SÁBADO 23 DE MARZO<br/>"+
   "PARQUE MÉXICO - DE 8:00AM A 2:00PM.</p>"+
-  "<p class='entre'>MARTES 25 DE FEBRERO<br/>"+
+  "<p class='entre'>MARTES 25 DE MARZO<br/>"+
   "ESIME CULHUACÁN Y PERICOAPA - DE 11:00AM A 6:00PM.</p>"+
-  "<p class='entre'>VIERNES 28 DE FEBRERO<br/>"+
+  "<p class='entre'>VIERNES 28 DE MARZO<br/>"+
   "PARQUE MÉXICO - DE 11:00AM A 6:00PM.</p>"+
-  "<p class='entre'>SÁBADO 1 DE MARZO<br/>"+
+  "<p class='entre'>SÁBADO 1 DE ABRIL<br/>"+
   "CERRO DE LA ESTRELLA - DE 8:00AM A 2:00PM.</p>"+
-  "<p class='entre'>DOMINGO 2 DE MARZO<br/>"+
-  "PARQUE LIRA - DE 8:00AM A 2:00PM.</p>"+
-  "<p class='entre'>MARTES 4 DE MARZO<br/>"+
-  "UNITEC ERMITA Y ENAP XOCHIMILCO - DE 11:00AM A 6:00PM.</p>"+
-  "<p class='entre'>JUEVES 6 DE MARZO<br/>"+
-  "CERRO DE LA ESTRELLA Y PREPA DON BOSCO - DE 11:00AM A 6:00PM.</p>"+
-  "<p class='entre'>VIERNES 7 DE MARZO<br/>"+
-  "TOUR CONDESA - DE 3:00PM A 9:00PM.</p>"+
   "</div></div>"; 
   var html = "<div id='sub_izq' class='retos_der responsive_bck'>" + html_izq + "</div><div id='sub_der' class='retos_der responsive_bck centered overflow_hidden'><img src='/assets/reto_truck.png' class='modal_reto' /></div>"; 
   modalDialogue(html);
